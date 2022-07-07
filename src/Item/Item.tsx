5c274de4e2ee70e7ef9392d8131dd84d2acf60fa
Ramazan Gülşen <97034453+ramazangulsen@users.noreply.github.com>
2022-07-07 16:20:21 +0300

    basket added

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -1,6 +1,6 @@
 import {Button} from '@material-ui/core'
 import { CartItemType } from '../App'
-import { Wrapper } from './Item.style'
+import { Wrapper } from './Item.styles'
 type Props = {
     item: CartItemType
     handleAddToCart: (clickedItem: CartItemType) => void
